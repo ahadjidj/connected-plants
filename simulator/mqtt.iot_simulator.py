84c703ef06de3bd0a8e67b190db196c6cf3476b6
ahadjidj <abdelkrim.hadjidj@gmail.com>
2019-10-02 11:56:02 +0200

    Updated simulator logic

--- simulator/mqtt.iot_simulator.py
+++ simulator/mqtt.iot_simulator.py
@@ -4,10 +4,9 @@ import sys
 import json
 import time
 import random
-
 import paho.mqtt.client as mqtt
 
-def generate(host, port, username, password, topic, machines, interval_ms, verbose):
+def generate(host, port, username, password, topic, machines, interval_ms, verbose, yield_ratio):
     """generate data and send it to an MQTT broker"""
     mqttc = mqtt.Client()
 
@@ -18,21 +17,25 @@ def generate(host, port, username, password, topic, machines, interval_ms, verbo
 
     interval_secs = interval_ms / 1000.0
 
+    random.seed()
+
     while True:
         data = {
-            "sensor_id": random.randint(1,100),
+            "machine_id": random.randint(1,100),
             "sensor_ts": long(time.time()*1000000)
         }
 
-        inject_error = False
-        if random.randint(1,100) >= 85:
-            inject_error = True
+        inject_error = (random.randint(1,100) >=  yield_ratio)
+        if inject_error:
+            data["sensor_0"] = random.randint(51,100)
+        else:
+            data["sensor_0"] = random.randint(0,50)
 
-        for key in range(0, 12):
+        for key in range(1, 11):
             min_val, max_val = machines.get("sensor_" + str(key))
             data["sensor_" + str(key)] = random.randint(min_val, max_val)
-            if inject_error and key < 2 and random.randint(1, 100) < 80:
-                data["sensor_" + str(key)] += random.randint(500,1000)
+            if inject_error and key < 3:
+                data["sensor_" + str(key)] *= random.randint(3,7)
 
         payload = json.dumps(data)
 
@@ -43,7 +46,7 @@ def generate(host, port, username, password, topic, machines, interval_ms, verbo
         time.sleep(interval_secs)
 
 
-def main(config_path):
+def main(config_path,plant_id,yield_ratio):
     """main entry point, load and validate config and call generate"""
     try:
         with open(config_path) as handle:
@@ -59,14 +62,15 @@ def main(config_path):
             port = mqtt_config.get("port", 1883)
             username = mqtt_config.get("username")
             password = mqtt_config.get("password")
-            topic = mqtt_config.get("topic", "mqttgen")
+            topic = mqtt_config.get("topic", "mqttgen") + "/" + plant_id
 
-            generate(host, port, username, password, topic, machines, interval_ms, verbose)
+            generate(host, port, username, password, topic, machines, interval_ms, verbose, int(yield_ratio))
+
     except IOError as error:
         print("Error opening config file '%s'" % config_path, error)
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        main(sys.argv[1])
+    if len(sys.argv) == 4:
+        main(sys.argv[1],sys.argv[2],sys.argv[3])
     else:
-        print("usage %s config.json" % sys.argv[0])
+        print("usage %s config.json plant_id yield_ratio" % sys.argv[0])
